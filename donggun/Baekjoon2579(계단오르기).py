109d7f3be6b1aa90ccf1b54edee3cd93ebf07d5a
DongGunYoon <donggun961108@gmail.com>
2021-04-15 18:48:31 +0900

    doc: Added the comment on code.

--- donggun/Baekjoon2579(계단오르기).py
+++ donggun/Baekjoon2579(계단오르기).py
@@ -1,10 +1,14 @@
+# 계단 수 입력
 n = int(input())
 
+# 각 계단의 점수 기록할 리스트
 points = []
 
+# 각 계단의 점수 입력
 for _ in range(n):
     points.append(int(input()))
 
+# dp테이블 초기화
 max_point = [0 for _ in range(n)]
 
 max_point[0] = points[0]
