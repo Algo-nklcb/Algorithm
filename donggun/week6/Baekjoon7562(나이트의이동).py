28ced9c444d66728755b032e79aacdf6689434cf
DongGunYoon <donggun961108@gmail.com>
2021-06-01 21:21:32 +0900

    feat: BOJ 7562

--- donggun/week6/Baekjoon7562(나이트의이동).py
+++ donggun/week6/Baekjoon7562(나이트의이동).py
@@ -1,22 +1,23 @@
 from collections import deque
 
-n = int(input())
+cases = int(input())
 
 dx = [-2, -2, -1, -1, 1, 1, 2, 2]
 dy = [1, -1, 2, -2, 2, -2, 1, -1]
 
-
-for i in range(n):
-    count = 0
-    queue = deque()
-    l = int(input())
+for _ in range(cases):
+    size = int(input())
     start_x, start_y = map(int, input().split())
-    queue.append((start_x, start_y))
     end_x, end_y = map(int, input().split())
-    graph = [[0] * n for _ in range(n)]
+    queue = deque([(start_x, start_y)])
+    graph = [[0] * size for _ in range(size)]
     
-
-    while (graph[end_x][end_y] == 0):
+    while (graph[end_x][end_y] == 0 and not((start_x, start_y) == (end_x, end_y))):
         pop_x, pop_y = queue.popleft()
         for i in range(8):
-            cur_x, cur_y = pop_x + dx[i], pop_y + dy[i]
+            moved_x, moved_y = pop_x + dx[i], pop_y + dy[i]
+            while (size > moved_x >= 0 and size > moved_y >= 0 and graph[moved_x][moved_y]==0):
+                graph[moved_x][moved_y] = graph[pop_x][pop_y] + 1
+                queue.append((moved_x, moved_y))
+    
+    print(graph[end_x][end_y])
